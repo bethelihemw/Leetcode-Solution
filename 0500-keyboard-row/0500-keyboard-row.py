6ab44f616b7e5729c38d3c118f8971f7cce6852a
Bethelihem Wondimneh <wondimnehbethelihem@gmail.com>
2025-11-27 23:33:59 +0300

    Time: 0 ms (100%), Space: 17.9 MB (10.21%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+
+        result = []
+
+        for word in words:
+            lower = set(word.lower())
+            if lower.issubset(row1) or lower.issubset(row2) or lower.issubset(row3):
+                result.append(word)
+        
+        return result
+        
